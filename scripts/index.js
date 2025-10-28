9c6e4e15b0a4909ed4e6bbfbf6db5f1fb2b16b32
ValD <valdalton333@gmail.com>
2025-10-28 13:22:49 -0600

    Update

--- scripts/index.js
+++ scripts/index.js
@@ -6,24 +6,23 @@ const initialCards = [
   {
     name: "Restaraunt Terrace",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
-  }
-   {
+  },
+  {
     name: "An Outdoor Cafe",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
-
-  }
-   {
+  },
+  {
     name: "A very long bridge, over the forest and through the trees",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-  }
-   {
+  },
+  {
     name: "Tunnel with morning light",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-  }
-   {
+  },
+  {
     name: "Mountain house",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-  }
+  },
 ];
 
 function openModal(modal) {
@@ -84,4 +83,4 @@ addCardFormEl.addEventListener("submit", (evt) => {
 
 initialCards.forEach(function (item) {
   console.log(item.name, item.link);
-})
+});
